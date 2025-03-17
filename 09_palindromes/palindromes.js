db7134c973e79bc1832534c3118d5f0f55c7f42e
Dante Dupont <dmxdupont@gmail.com>
2025-03-16 21:39:40 -0400

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (palindrome) {
+    let removePunc = palindrome.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ]/g, '').toUpperCase();
+    let mirror = removePunc.split('').reverse().join('');
+    if (removePunc === mirror) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
