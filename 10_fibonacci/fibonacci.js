95f2ac26ba6e4978f414226375ec08723c588a82
Dante Dupont <dmxdupont@gmail.com>
2025-03-16 22:02:36 -0400

    Complete fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = +num;
+    let fibonacciArr = [1, 1,];
+    if (num < 0){
+        return 'OOPS';
+    } else if (num === 0){
+        return 0;
+    } else {
+        for (let i = 2; i < num; i++){
+            fibonacciArr = fibonacciArr.concat((fibonacciArr[i-1] + fibonacciArr[i-2]));
+        }
+        return fibonacciArr[num-1];
+    }
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
