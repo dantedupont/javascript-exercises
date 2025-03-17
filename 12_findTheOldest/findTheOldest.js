181cbe5f667364ed40915e1f72b0593fc9f275f3
Dante Dupont <dmxdupont@gmail.com>
2025-03-16 22:36:19 -0400

    Complete functino to find the oldest person in arr

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear = (new Date()).getFullYear();
+    const ages = people.sort(function(a,b) {
+        if (!a.yearOfDeath) a.yearOfDeath = currentYear;
+        if (!b.yearOfDeath) b.yearOfDeath = currentYear;
+        const lastPersonAge = a.yearOfDeath - a.yearOfBirth;
+        const nextPersonAge = b.yearOfDeath - b.yearOfBirth;
+        return lastPersonAge > nextPersonAge ? -1 : 1;
+    });
+    return ages[0];
 };
 
 // Do not edit below this line
